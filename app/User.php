77004529e9bbde0aac9233e962384f6b66a23917
Ismael Lastre Alvarez <ismaelfac@gmail.com>
2020-09-08 17:12:24 -0500

    Base Project

---
+++ app/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Caffeinated\Shinobi\Concerns\HasRolesAndPermissions;
+
+class User extends Authenticatable
+{
+    use Notifiable, SoftDeletes,HasRolesAndPermissions;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'state', 'is_active'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'state' => 'boolean',
+        'is_active' => 'boolean'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function isInactive()
+    {
+        return $this->state === false;
+    }
+
+    public function owns(Model $model)
+    {
+        return $this->id === $model->user_id;
+    }
+
+    public function scopeName($query, $name)
+    {
+        if (trim($name) != "") {
+            $query->where('name', 'LIKE', '%' . $name . '%');
+        }
+    }
+    public function scopeEmail($query, $email)
+    {
+        if (trim($email) != "") {
+            $query->where('email', 'LIKE', '%' . $email . '%');
+        }
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        $this->truncateTables([
+            'password_resets',
+            'users',
+            'permission_role',
+            'permissions',
+            'roles',
+            'role_user',
+            'customers',
+            'identifications',
+            'neighborhoods',
+            'locations',
+            'municipalities',
+            'departaments',
+            'countries',
+            'identifications'
+        ]);
+        $this->call([
+            PermissionsTableSeeder::class,
+            CountryTableSeeder::class,
+            DepartamentTableSeeder::class,
+            MunicipalityTableSeeder::class,
+            LocationTableSeeder::class,
+            NeighborhoodTableSeeder::class,
+            IdentificationTableSeeder::class,
+            CivilstatusTableSeeder::class,
+            CustomersTableSeeder::class,
+            UserTableSeeder::class
+        ]);
+    }
+
+    protected function truncateTables(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); // Desactivamos la revisión de claves foráneas
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); // Reactivamos la revisión de claves foráneas
+    }
+}
+//cperez@solubienes.com
