1e1fc8542558b8e06fb40c08e19aadc787a60c28
Ismael Lastre Alvarez <ismaelfac@gmail.com>
2020-09-09 21:21:18 -0500

    Base Project

--- app/Models/System/CompanyModule.php
+++ app/Models/System/CompanyModule.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Models\System;
 
 use Illuminate\Database\Eloquent\Model;
 
 class CompanyModule extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ['company_id','module_id'];
+    protected $casts = [];
 }
